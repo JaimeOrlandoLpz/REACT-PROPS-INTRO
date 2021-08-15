0d3d9f3439055f47587f577b73c71b1030615eff
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-08-15 14:58:36 -0500

    Final

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img
+        style={{ maxHeight: "10rem", maxWidth: "15rem" }}
+        src={props.imgSrc}
+        alt="avatar_img"
+      />
+      <p>Tel: {props.tel}</p>
+      <p>Mail: {props.mail}</p>
+    </div>
+  );
+}
+
 ReactDOM.render(
   <div>
     <h1>My Contacts</h1>
-
-    <h2>Beyonce</h2>
-    <img
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      alt="avatar_img"
+    <Card
+      name="Beyonce"
+      imgSrc="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      tel="+187513263"
+      mail="be.beyonce@beyonceCO.com"
     />
-    <p>+123 456 789</p>
-    <p>b@beyonce.com</p>
 
-    <h2>Jack Bauer</h2>
-    <img
-      src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      alt="avatar_img"
+    <Card
+      name="Popola"
+      imgSrc="https://xboxplay.games/uploadStream/16343.jpg"
+      tel="+122474487139..."
+      mail="popola_twin.town-library@projectgestalt.nier"
     />
-    <p>+987 654 321</p>
-    <p>jack@nowhere.com</p>
-
-    <h2>Chuck Norris</h2>
-    <img
-      src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      alt="avatar_img"
-    />
-    <p>+918 372 574</p>
-    <p>gmail@chucknorris.com</p>
   </div>,
   document.getElementById("root")
 );
